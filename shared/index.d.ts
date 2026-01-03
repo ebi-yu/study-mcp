bdca58d1472d4ab35332ea8632fd53c92118ea17
ebi-yu <ebiDevelopper1088@gmail.com>
2026-01-03 12:09:42 +0900

    refactor : 余計なファイルの削除

--- shared/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { McpResponse, type McpToolResponse } from "./McpResponse";
-export { ResultHelper, type Result } from "./Result";
-//# sourceMappingURL=index.d.ts.map
--- shared/index.js
+++
@@ -1,3 +0,0 @@
-export { McpResponse } from "./McpResponse";
-export { ResultHelper } from "./Result";
-//# sourceMappingURL=index.js.map
